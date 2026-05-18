c9fc504b33292cec9de4fd39caffa11ac57177ff
ZhangShimin1 <stevezhang886@gmail.com>
2026-05-18 12:00:54 +0000

    try push

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ version_folder = os.path.dirname(os.path.join(os.path.abspath(__file__)))
 with open(os.path.join(version_folder, "verl/version/version")) as f:
     __version__ = f.read().strip()
 
+
 install_requires = [
     "accelerate",
     "codetiming",
